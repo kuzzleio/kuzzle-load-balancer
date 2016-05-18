6ab974c3392cc1561f0f8790934ead9a219d444e
David Bengsch <dbengsch@kaliop.com>
2016-05-18 14:42:04 +0200

    WIP : Refactor in progress

--- bin/load-balancer.js
+++ bin/load-balancer.js
@@ -1,24 +1,58 @@
 var
-  broker = new (require('../lib/Broker.js'))(),
-  config = require('rc')('lb', {}),
+  Broker = require('../lib/Broker.js'),
+  config = require('rc')('lb', {
+    protocolPlugins: {},
+    serverMode: 'failover',
+    serverOptions: {}
+  }),
   isDummy = false,
-  context = new (require('../lib/Context.js'))(broker),
-  plugins = {};
+  Context = require('../lib/Context.js'),
+  context,
+  broker,
+  activatedPlugins,
+  protocolPlugins = config.protocolPlugins;
 
-if (Object.keys(config.protocolPlugins).length > 0) {
-  Object.keys(config.protocolPlugins).forEach((pluginName) => {
-    var
-      pluginObject,
-      pluginConfig = config.protocolPlugins[pluginName];
-    if (pluginConfig.activated) {
-      console.log(`Initializing plugin protocol ${pluginName}`);
-      pluginObject = new (require(pluginName))();
-      plugins[pluginObject.protocol] = pluginObject.init(pluginConfig.config, context, isDummy);
+context = new Context();
+broker = new Broker();
+
+context.setBroker(broker);
+
+if (Object.keys(protocolPlugins).length > 0) {
+  activatedPlugins = Object
+    .keys(protocolPlugins)
+    .map(pluginName => protocolPlugins[pluginName])
+    .filter(pluginConfig => pluginConfig.activated);
+
+  if (activatedPlugins.length > 0) {
+    Object.keys(protocolPlugins).forEach((pluginName) => {
+      var
+        plugin,
+        pluginConfig = protocolPlugins[pluginName];
+
+      if (pluginConfig.activated) {
+        console.log(`Initializing plugin protocol ${pluginName}`);
+        try {
+          plugin = new (require(pluginName))();
+          plugin.init(pluginConfig.config, context, isDummy);
+
+          context.pluginStore.add(plugin);
+        } catch (error) {
+          console.error(`Initialization of plugin ${pluginName} has failed; Reason :`, error);
+        }
+      }
+    });
+
+    if (context.pluginStore.count()) {
+      console.error('No plugin has been initialized properly. We shutdown.');
+      process.exit(1);
     }
-  });
+  } else {
+    console.error('No plugin has been activated in configuration. We shutdown.');
+    process.exit(1);
+  }
 
-  broker.initializeBroker(config.serverMode, config.serverOptions, plugins);
+  broker.init(context, config.serverMode, config.serverOptions);
 } else {
-  console.error('No plugin configuration provided');
+  console.error('No plugin configuration provided. We shutdown.');
   process.exit(1);
 }
