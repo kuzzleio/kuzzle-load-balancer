ec53e06c7f0dc51ac9875671928001f08b4c51c3
David Bengsch <dbengsch@kaliop.com>
2016-06-06 10:02:31 +0200

    More renaming in tests and package.json file

--- test/core/kuzzleProxy.test.js
+++ test/core/kuzzleProxy.test.js
@@ -3,7 +3,7 @@ var
   should = require('should'),
   rewire = require('rewire'),
   path = require('path'),
-  LoadBalancer = rewire('../../lib/core/KuzzleProxy'),
+  KuzzleProxy = rewire('../../lib/core/KuzzleProxy'),
   Broker = require.main.require('lib/service/Broker'),
   HttpProxy = require.main.require('lib/service/HttpProxy'),
   Context = require.main.require('lib/core/Context');
@@ -50,7 +50,7 @@ describe('Test: core/KuzzleProxy', function () {
     };
 
   before(() => {
-    LoadBalancer.__set__('console', {log: () => {}, error: () => {}});
+    KuzzleProxy.__set__('console', {log: () => {}, error: () => {}});
   });
 
   beforeEach(() => {
@@ -58,7 +58,7 @@ describe('Test: core/KuzzleProxy', function () {
     serverMode = 'failover';
     protocolPlugins = {};
 
-    sandbox.stub(LoadBalancer.prototype, 'getRCConfig', () => {
+    sandbox.stub(KuzzleProxy.prototype, 'getRCConfig', () => {
       return {serverMode, protocolPlugins, serverTimeout};
     });
 
@@ -70,7 +70,7 @@ describe('Test: core/KuzzleProxy', function () {
       return require(requireArgument);
     });
 
-    LoadBalancer.__set__('require', requireStub);
+    KuzzleProxy.__set__('require', requireStub);
   });
 
   afterEach(() => {
@@ -78,52 +78,52 @@ describe('Test: core/KuzzleProxy', function () {
   });
 
   it('constructor must initialize internal members', () => {
-    var loadBalancer;
+    var proxy;
 
-    loadBalancer = new LoadBalancer();
+    proxy = new KuzzleProxy();
 
-    should(loadBalancer.context).be.instanceOf(Context);
-    should(loadBalancer.broker).be.instanceOf(Broker);
-    should(loadBalancer.httpProxy).be.instanceOf(HttpProxy);
-    should(loadBalancer.isDummy).be.false();
-    should(loadBalancer.config).be.an.Object();
+    should(proxy.context).be.instanceOf(Context);
+    should(proxy.broker).be.instanceOf(Broker);
+    should(proxy.httpProxy).be.instanceOf(HttpProxy);
+    should(proxy.isDummy).be.false();
+    should(proxy.config).be.an.Object();
   });
 
   it('constructor must throw an error if serverMode is bad', () => {
     serverMode = 'unknown';
 
     (function() {
-      new LoadBalancer();
+      new KuzzleProxy();
     }).should.throw('Server mode option must be either set to "failover" or "round-robin"; "unknown" given');
   });
 
   it('method getRCConfig must return an object', () => {
-    var loadBalancer;
+    var proxy;
 
     sandbox.restore();
 
-    loadBalancer = new LoadBalancer();
+    proxy = new KuzzleProxy();
 
-    should(loadBalancer.getRCConfig()).be.an.Object();
+    should(proxy.getRCConfig()).be.an.Object();
   });
 
   it('method initPlugins must initialize plugins when activated', () => {
     var
-      loadBalancer,
+      proxy,
       pluginStoreAddStub;
 
-    sandbox.stub(LoadBalancer.prototype, 'readPluginsConfiguration').returns(dummyActivatedPlugin);
+    sandbox.stub(KuzzleProxy.prototype, 'readPluginsConfiguration').returns(dummyActivatedPlugin);
     sandbox
-      .stub(LoadBalancer.prototype, 'requirePluginPackage')
+      .stub(KuzzleProxy.prototype, 'requirePluginPackage')
       .withArgs(aPluginName).returns(dummyPluginConstructor(aPluginName))
       .withArgs(anotherPluginName).returns(dummyPluginConstructor(anotherPluginName));
 
-    loadBalancer = new LoadBalancer();
+    proxy = new KuzzleProxy();
 
-    pluginStoreAddStub = sandbox.stub(loadBalancer.context.pluginStore, 'add');
-    sandbox.stub(loadBalancer.context.pluginStore, 'count').returns(2);
+    pluginStoreAddStub = sandbox.stub(proxy.context.pluginStore, 'add');
+    sandbox.stub(proxy.context.pluginStore, 'count').returns(2);
 
-    loadBalancer.initPlugins();
+    proxy.initPlugins();
 
     should(pluginStoreAddStub.getCall(0).args[0].pluginName).be.eql(aPluginName);
     should(pluginStoreAddStub.getCall(1).args[0].pluginName).be.eql(anotherPluginName);
@@ -131,71 +131,71 @@ describe('Test: core/KuzzleProxy', function () {
   });
 
   it('method initPlugins must initialize plugins when activated', () => {
-    var loadBalancer;
+    var proxy;
 
-    sandbox.stub(LoadBalancer.prototype, 'readPluginsConfiguration').returns(dummyMixedPlugin);
-    sandbox.stub(LoadBalancer.prototype, 'requirePluginPackage').returns(dummyPluginConstructor(aPluginName));
+    sandbox.stub(KuzzleProxy.prototype, 'readPluginsConfiguration').returns(dummyMixedPlugin);
+    sandbox.stub(KuzzleProxy.prototype, 'requirePluginPackage').returns(dummyPluginConstructor(aPluginName));
 
-    loadBalancer = new LoadBalancer();
+    proxy = new KuzzleProxy();
 
-    loadBalancer.initPlugins();
+    proxy.initPlugins();
 
-    should(loadBalancer.context.pluginStore.count()).be.eql(1);
+    should(proxy.context.pluginStore.count()).be.eql(1);
   });
 
   it('method initPlugins must throw an error if no plugin is initialized', () => {
-    var loadBalancer;
+    var proxy;
 
-    sandbox.stub(LoadBalancer.prototype, 'readPluginsConfiguration').returns({});
+    sandbox.stub(KuzzleProxy.prototype, 'readPluginsConfiguration').returns({});
 
-    loadBalancer = new LoadBalancer();
+    proxy = new KuzzleProxy();
 
-    loadBalancer.initPlugins.bind(loadBalancer).should.throw('No plugin has been initialized properly. Shutting down.');
+    proxy.initPlugins.bind(proxy).should.throw('No plugin has been initialized properly. Shutting down.');
   });
 
   it('method initPlugins must recover on an error if plugin does not initialize properly', () => {
     var
-      loadBalancer,
+      proxy,
       pluginStoreAddStub;
 
-    sandbox.stub(LoadBalancer.prototype, 'readPluginsConfiguration').returns(dummyActivatedPlugin);
+    sandbox.stub(KuzzleProxy.prototype, 'readPluginsConfiguration').returns(dummyActivatedPlugin);
     sandbox
-      .stub(LoadBalancer.prototype, 'requirePluginPackage')
+      .stub(KuzzleProxy.prototype, 'requirePluginPackage')
       .withArgs(anotherPluginName).returns(dummyPluginConstructor(anotherPluginName))
       .withArgs(aPluginName).throws(Error);
 
-    loadBalancer = new LoadBalancer();
-    pluginStoreAddStub = sandbox.stub(loadBalancer.context.pluginStore, 'add');
-    sandbox.stub(loadBalancer.context.pluginStore, 'count').returns(1);
+    proxy = new KuzzleProxy();
+    pluginStoreAddStub = sandbox.stub(proxy.context.pluginStore, 'add');
+    sandbox.stub(proxy.context.pluginStore, 'count').returns(1);
 
-    loadBalancer.initPlugins();
+    proxy.initPlugins();
 
     should(pluginStoreAddStub.getCall(0).args[0].pluginName).be.eql(anotherPluginName);
     should(pluginStoreAddStub.callCount).be.eql(1);
   });
 
   it('method initBroker initializes the broker', () => {
-    var loadBalancer = new LoadBalancer();
-    var initBrokerStub = sandbox.stub(loadBalancer.broker, 'init');
+    var proxy = new KuzzleProxy();
+    var initBrokerStub = sandbox.stub(proxy.broker, 'init');
 
-    loadBalancer.initBroker();
+    proxy.initBroker();
 
-    should(initBrokerStub.calledWith(loadBalancer.config.serverMode, loadBalancer.context, loadBalancer.config.serverOptions, loadBalancer.config.serverTimeout)).be.true();
-    should(loadBalancer.context.broker).be.eql(loadBalancer.broker);
+    should(initBrokerStub.calledWith(proxy.config.serverMode, proxy.context, proxy.config.serverOptions, proxy.config.serverTimeout)).be.true();
+    should(proxy.context.broker).be.eql(proxy.broker);
   });
 
   it('method initHttpProxy initializes the httpProxy', () => {
-    var loadBalancer = new LoadBalancer();
-    var iniProxyStub = sandbox.stub(loadBalancer.httpProxy, 'init');
+    var proxy = new KuzzleProxy();
+    var iniProxyStub = sandbox.stub(proxy.httpProxy, 'init');
 
-    loadBalancer.initHttpProxy();
-    should(iniProxyStub.calledWith(loadBalancer.config.serverMode, loadBalancer.context, loadBalancer.config.httpPort)).be.true();
+    proxy.initHttpProxy();
+    should(iniProxyStub.calledWith(proxy.config.serverMode, proxy.context, proxy.config.httpPort)).be.true();
   });
 
 
   it('method readPluginsConfiguration must return the configuration of the plugins', () => {
     var
-      loadBalancer,
+      proxy,
       currentConfig = {},
       readOnePluginConfigurationStub,
       loadCurrentConfigStub;
@@ -205,109 +205,109 @@ describe('Test: core/KuzzleProxy', function () {
       anotherPluginName
     };
 
-    loadBalancer = new LoadBalancer();
+    proxy = new KuzzleProxy();
 
-    readOnePluginConfigurationStub = sandbox.stub(LoadBalancer.prototype, 'readOnePluginConfiguration');
-    loadCurrentConfigStub = sandbox.stub(LoadBalancer.prototype, 'loadCurrentConfig');
+    readOnePluginConfigurationStub = sandbox.stub(KuzzleProxy.prototype, 'readOnePluginConfiguration');
+    loadCurrentConfigStub = sandbox.stub(KuzzleProxy.prototype, 'loadCurrentConfig');
 
     readOnePluginConfigurationStub.returns({activated: true});
     loadCurrentConfigStub.returns(currentConfig);
 
-    should(Object.keys(loadBalancer.readPluginsConfiguration()).length).be.eql(2);
+    should(Object.keys(proxy.readPluginsConfiguration()).length).be.eql(2);
     should(readOnePluginConfigurationStub.calledTwice).be.true();
   });
-0
+
   it('method readPluginsConfiguration throw an error if no plugin is activated', () => {
     var
-      loadBalancer,
+      proxy,
       currentConfig = {},
       readOnePluginConfigurationStub,
       loadCurrentConfigStub;
 
-    loadBalancer = new LoadBalancer();
+    proxy = new KuzzleProxy();
 
-    readOnePluginConfigurationStub = sandbox.stub(LoadBalancer.prototype, 'readOnePluginConfiguration');
-    loadCurrentConfigStub = sandbox.stub(LoadBalancer.prototype, 'loadCurrentConfig');
+    readOnePluginConfigurationStub = sandbox.stub(KuzzleProxy.prototype, 'readOnePluginConfiguration');
+    loadCurrentConfigStub = sandbox.stub(KuzzleProxy.prototype, 'loadCurrentConfig');
 
     readOnePluginConfigurationStub.returns({activated: false});
     loadCurrentConfigStub.returns(currentConfig);
 
-    loadBalancer.config.protocolPlugins = {
+    proxy.config.protocolPlugins = {
       aPluginName,
       anotherPluginName
     };
 
-    loadBalancer.readPluginsConfiguration.bind(loadBalancer).should.throw('No plugin has been activated in configuration. Shutting down.');
+    proxy.readPluginsConfiguration.bind(proxy).should.throw('No plugin has been activated in configuration. Shutting down.');
   });
 
   it('method readPluginsConfiguration throws an Error if no plugin configuration is provided', () => {
-    var loadBalancer;
+    var proxy;
 
-    loadBalancer = new LoadBalancer();
+    proxy = new KuzzleProxy();
 
-    loadBalancer.readPluginsConfiguration.bind(loadBalancer).should.throw('No plugin configuration provided. Shutting down.');
+    proxy.readPluginsConfiguration.bind(proxy).should.throw('No plugin configuration provided. Shutting down.');
   });
 
   it('method requirePluginPackage must return required item', () => {
-    var loadBalancer = new LoadBalancer();
-    var getPathPluginStub = sandbox.stub(LoadBalancer.prototype, 'getPathPlugin').returns('dummy');
+    var proxy = new KuzzleProxy();
+    var getPathPluginStub = sandbox.stub(KuzzleProxy.prototype, 'getPathPlugin').returns('dummy');
 
-    loadBalancer.requirePluginPackage('dummy');
+    proxy.requirePluginPackage('dummy');
 
     should(getPathPluginStub.calledWith('dummy')).be.true();
     should(requireStub.calledWith('dummy')).be.true();
   });
 
   it('method requirePluginConfig must return required item', () => {
-    var loadBalancer = new LoadBalancer();
-    var getPathPluginStub = sandbox.stub(LoadBalancer.prototype, 'getPathPlugin').returns('dummy');
+    var proxy = new KuzzleProxy();
+    var getPathPluginStub = sandbox.stub(KuzzleProxy.prototype, 'getPathPlugin').returns('dummy');
 
-    loadBalancer.requirePluginConfig('dummy');
+    proxy.requirePluginConfig('dummy');
 
     should(getPathPluginStub.calledWith('dummy')).be.true();
     should(requireStub.calledWith(path.join('dummy', 'package.json'))).be.true();
   });
 
   it('method getPathPlugin must return a path from path property if it exists', () => {
-    var loadBalancer;
+    var proxy;
     protocolPlugins = {
       dummy: {
         path: 'a-path'
       }
     };
 
-    loadBalancer = new LoadBalancer();
+    proxy = new KuzzleProxy();
 
-    should(loadBalancer.getPathPlugin('dummy')).be.eql('a-path');
+    should(proxy.getPathPlugin('dummy')).be.eql('a-path');
   });
 
   it('method getPathPlugin must return a path from node_module if no path defined in configuration', () => {
-    var loadBalancer;
+    var proxy;
     protocolPlugins = {
       dummy: {}
     };
 
-    loadBalancer = new LoadBalancer();
+    proxy = new KuzzleProxy();
 
-    should(loadBalancer.getPathPlugin('dummy')).be.eql(path.join(__dirname, '..', '..', 'node_modules', 'dummy'));
+    should(proxy.getPathPlugin('dummy')).be.eql(path.join(__dirname, '..', '..', 'node_modules', 'dummy'));
   });
 
   it('method loadCurrentConfig must return the configuration', () => {
     var
-      loadBalancer,
+      proxy,
       jsonString = '{"dummy": "config"}',
       existsSyncStub,
       readFileSyncStub,
       jsonPluginsConfigPath;
 
-    fs = LoadBalancer.__get__('fs');
-    jsonPluginsConfigPath = LoadBalancer.__get__('jsonPluginsConfigPath');
+    fs = KuzzleProxy.__get__('fs');
+    jsonPluginsConfigPath = KuzzleProxy.__get__('jsonPluginsConfigPath');
     existsSyncStub = sinon.stub(fs, 'existsSync').returns(true);
     readFileSyncStub = sinon.stub(fs, 'readFileSync').returns(jsonString);
 
-    loadBalancer = new LoadBalancer();
+    proxy = new KuzzleProxy();
 
-    should(loadBalancer.loadCurrentConfig()).be.deepEqual(JSON.parse(jsonString));
+    should(proxy.loadCurrentConfig()).be.deepEqual(JSON.parse(jsonString));
     should(existsSyncStub.calledWith(jsonPluginsConfigPath)).be.true();
     should(readFileSyncStub.calledWith(jsonPluginsConfigPath)).be.true();
 
@@ -317,17 +317,17 @@ describe('Test: core/KuzzleProxy', function () {
 
   it('method loadCurrentConfig must return an empty object if configuration does not exist', () => {
     var
-      loadBalancer,
+      proxy,
       existsSyncStub,
       jsonPluginsConfigPath;
 
-    fs = LoadBalancer.__get__('fs');
-    jsonPluginsConfigPath = LoadBalancer.__get__('jsonPluginsConfigPath');
+    fs = KuzzleProxy.__get__('fs');
+    jsonPluginsConfigPath = KuzzleProxy.__get__('jsonPluginsConfigPath');
     existsSyncStub = sinon.stub(fs, 'existsSync').returns(false);
 
-    loadBalancer = new LoadBalancer();
+    proxy = new KuzzleProxy();
 
-    should(loadBalancer.loadCurrentConfig()).be.deepEqual({});
+    should(proxy.loadCurrentConfig()).be.deepEqual({});
     should(existsSyncStub.calledWith(jsonPluginsConfigPath)).be.true();
 
     existsSyncStub.restore();
