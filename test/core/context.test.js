4674cfb9c2ae314064456e54410e0a3f65a436b6
scottinet <scottinet@kaliop.com>
2016-06-16 12:22:49 +0200

    added unit test on new context format

--- test/core/context.test.js
+++ test/core/context.test.js
@@ -4,6 +4,7 @@ var
   ClientConnectionStore = require.main.require('lib/store/ClientConnection'),
   PluginStore = require.main.require('lib/store/Plugin'),
   sinon = require('sinon'),
+  rewire = require('rewire'),
   Router = require.main.require('lib/service/Router');
 
 describe('Test: core/Context', function () {
@@ -47,5 +48,19 @@ describe('Test: core/Context', function () {
     var context = new Context(BackendHandler, backendMode);
 
     should(context.accessors.router).be.deepEqual(context.router);
+  });
+
+  it('should dynamically add kuzzle error constructors', () => {
+    var
+      RewiredContext = rewire('../../lib/core/Context'),
+      context,
+      fakeErrorObject =  {
+        'fooBar': function () {}
+      };
+
+    RewiredContext.__set__('Errors', fakeErrorObject);
+
+    context = new RewiredContext(BackendHandler, backendMode);
+    should(context.errors).have.property('FooBar', fakeErrorObject.fooBar);
   });
 });
