010c10588966783aa15a99c370e6b3a4469e89c0
scottinet <scottinet@kaliop.com>
2016-07-27 11:30:34 +0200

    improvements

--- index.js
+++ index.js
@@ -3,20 +3,6 @@ var
   BackendHandler = require('./lib/service/ProxyBackendHandler'),
   proxy,
   applicationName = 'proxy';
-/*
-var pmx=  require('pmx').init({
-    http          : true, // HTTP routes logging (default: true)
-    ignore_routes : [/notFound/], // Ignore http routes with this pattern (Default: [])
-    errors        : true, // Exceptions loggin (default: true)
-    custom_probes : true, // Auto expose JS Loop Latency and HTTP req/s as custom metrics
-    network       : true, // Network monitoring at the application level
-    ports         : true,  // Shows which ports your app is listening on (default: false)
-    profiling : true
-  });
-*/
-console.log('==========================');
-
-console.log('Starting proxy instance');
 
 try {
   proxy = new KuzzleProxy(BackendHandler, applicationName);
