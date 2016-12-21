873c94bf153f1cda13da9d608358348c34d6352e
Benoît Vidis <bvidis@kuzzle.io>
2016-12-21 18:42:55 +0100

    tests wip

--- test/core/context.test.js
+++ test/core/context.test.js
@@ -1,66 +1,37 @@
-var
+'use strict';
+
+const
+  errors = require('kuzzle-common-objects').errors,
   should = require('should'),
-  Context = require.main.require('lib/core/Context'),
-  ClientConnectionStore = require.main.require('lib/store/ClientConnection'),
-  PluginStore = require.main.require('lib/store/Plugin'),
+  ClientConnection = require('../../lib/core/clientConnection'),
+  Context = require('../../lib/core/Context'),
   sinon = require('sinon'),
   rewire = require('rewire'),
+  Request = require('kuzzle-common-objects').Request,
   Router = require.main.require('lib/service/Router');
 
 describe('Test: core/Context', function () {
-  var
-    dummyBroker = {dummy: 'broker'},
-    backendMode = 'aMode',
-    BackendHandler,
-    sandbox;
-
-  before(() => {
-    sandbox = sinon.sandbox.create();
-    BackendHandler = sandbox.spy(function (mode) {
-      should(mode).be.eql(backendMode);
-    });
-  });
-
-  afterEach(() => {
-    sandbox.restore();
-  });
 
   it('constructor must initialize internal members', () => {
-    var context = new Context(BackendHandler, backendMode);
+    const context = new Context({foo: 'bar'});
 
-    should(context.broker).be.eql(null);
-    should(context.clientConnectionStore).be.instanceOf(ClientConnectionStore);
-    should(BackendHandler.calledWithNew()).be.true();
-    should(BackendHandler.calledWith(backendMode)).be.true();
-    should(context.pluginStore).be.instanceOf(PluginStore);
-    should(context.router).be.instanceOf(Router);
-  });
-
-  it('method setBroker must set broker accordingly', () => {
-    var context = new Context(BackendHandler, backendMode);
-
-    context.setBroker(dummyBroker);
-
-    should(context.broker).be.deepEqual(dummyBroker);
+    should(context.constructors.ClientConnection)
+      .be.exactly(ClientConnection);
+    should(context.constructors.Request)
+      .be.exactly(Request);
+    should(context.errors)
+      .be.exactly(errors);
   });
 
   it('method getRouter must return the router', () => {
-    var context = new Context(BackendHandler, backendMode);
+    const
+      proxy = {
+        router: {foo: 'bar'}
+      },
+      context = new Context(proxy);
 
-    should(context.accessors.router).be.deepEqual(context.router);
+    should (context.accessors.router)
+      .be.exactly(proxy.router);
   });
 
-  it('should dynamically add kuzzle error constructors', () => {
-    var
-      RewiredContext = rewire('../../lib/core/Context'),
-      context,
-      fakeErrorObject = {
-        'FooBar': function () {}
-      };
-
-    RewiredContext.__set__('errors', fakeErrorObject);
-
-    context = new RewiredContext(BackendHandler, backendMode);
-    should(context.errors).have.property('FooBar', fakeErrorObject.FooBar);
-  });
 });
--- test/core/kuzzleProxy.test.js
+++ test/core/kuzzleProxy.test.js
@@ -1,73 +1,107 @@
-var
+'use strict';
+
+const
+  _ = require('lodash'),
   rewire = require('rewire'),
   should = require('should'),
   sinon = require('sinon'),
   Promise = require('bluebird'),
-  KuzzleProxy = rewire('../../lib/core/KuzzleProxy');
+  KuzzleProxy = rewire('../../lib/core/KuzzleProxy'),
+  proxyConfig = require('../../lib/core/config');
 
 describe('lib/core/KuzzleProxy', () => {
-  var
-    backendHandler = {foo: 'bar'},
+  let
+    BackendHandler = sinon.spy(),
     reset,
-    proxy;
+    proxy,
+    winstonTransportConsole,
+    winstonTransportFile,
+    winstonTransportElasticsearch,
+    winstonTransportSyslog;
 
   beforeEach(() => {
+    winstonTransportConsole = sinon.spy();
+    winstonTransportElasticsearch = sinon.spy();
+    winstonTransportFile = sinon.spy();
+    winstonTransportSyslog = sinon.spy();
+
     reset = KuzzleProxy.__set__({
-      console: {
-        log: sinon.spy(),
-        error: sinon.spy()
-      },
+      config: _.cloneDeep(proxyConfig),
       Broker: sinon.spy(function () {
         this.init = sinon.spy();                // eslint-disable-line no-invalid-this
       }),
-      Context: sinon.spy(function (backend, mode) {
-        this.backend = backend;                 // eslint-disable-line no-invalid-this
-        this.mode = mode;                       // eslint-disable-line no-invalid-this
-        this.pluginStore = {                    // eslint-disable-line no-invalid-this
-          add: sinon.spy(),
-          count: sinon.stub().returns(2)
-        };
-        this.setBroker = sinon.spy();           // eslint-disable-line no-invalid-this
-      }),
+      Context: sinon.spy(),
       HttpProxy: sinon.spy(function () {
         this.init = sinon.spy();                // eslint-disable-line no-invalid-this
       }),
       PluginPackage: sinon.spy(function () {
         this.needsInstall = sinon.stub().returns(true);   // eslint-disable-line no-invalid-this
         this.install = sinon.stub().returns(Promise.resolve((function () { return this; })())); // eslint-disable-line no-invalid-this
-      })
+      }),
+      winston: {
+        Logger: sinon.spy(),
+        transports: {
+          Console: winstonTransportConsole,
+          File: winstonTransportFile
+        }
+      },
+      WinstonElasticsearch: winstonTransportElasticsearch,
+      WinstonSyslog: winstonTransportSyslog
     });
 
-    proxy = new KuzzleProxy(backendHandler);
+    proxy = new KuzzleProxy(BackendHandler);
+
+    Object.defineProperty(proxy, 'log', {
+      enumerable: true,
+      value: {
+        info: sinon.spy(),
+        warn: sinon.spy(),
+        error: sinon.spy()
+      }
+    });
   });
 
   afterEach(() => {
     reset();
   });
 
+  describe('#log getter', () => {
+    it('should return the error logger', () => {
+      proxy = new KuzzleProxy(BackendHandler);
+      proxy.loggers = {
+        errors: {foo: 'bar'}
+      };
+
+      should(proxy.log)
+        .be.exactly(proxy.loggers.errors);
+    });
+  });
+
   describe('#start', () => {
     it('should call proper methods in order', () => {
+      proxy.initLogger = sinon.spy();
       proxy.installPluginsIfNeeded = sinon.stub().returns(Promise.resolve());
       proxy.initPlugins = sinon.spy();
-      proxy.initBroker = sinon.spy();
-      proxy.initHttpProxy = sinon.spy();
 
       return proxy.start()
         .then(() => {
+          should(proxy.initLogger)
+            .be.calledOnce();
           should(proxy.installPluginsIfNeeded)
             .be.calledOnce();
           should(proxy.initPlugins)
             .be.calledOnce();
-          should(proxy.initBroker)
+          should(proxy.broker.init)
             .be.calledOnce();
-          should(proxy.initHttpProxy)
+          should(proxy.httpProxy.init)
             .be.calledOnce();
 
           sinon.assert.callOrder(
+            proxy.initLogger,
             proxy.installPluginsIfNeeded,
             proxy.initPlugins,
-            proxy.initBroker,
-            proxy.initHttpProxy
+            proxy.broker.init,
+            proxy.httpProxy.init
           );
         });
     });
@@ -90,9 +124,23 @@ describe('lib/core/KuzzleProxy', () => {
   });
 
   describe('#initPlugins', () => {
+    it ('should not installed plugins not marked as active', () => {
+      proxy.config.protocolPlugins = {
+        foo: {
+          activated: false
+        }
+      };
+
+      proxy.initPlugins();
+
+      should(proxy.log.info)
+        .have.callCount(0);
+    });
+
     it('should init plugins', () => {
-      var
+      const
         pkg = {
+          protocol: 'protocol',
           init: sinon.spy()
         };
 
@@ -112,66 +160,250 @@ describe('lib/core/KuzzleProxy', () => {
           .be.calledOnce()
           .be.calledWith(proxy.config.protocolPlugins.foo.config, proxy.context);
 
-        should(proxy.context.pluginStore.add)
-          .be.calledOnce()
-          .be.calledWith(pkg);
+        should(proxy.pluginStore.getByProtocol('protocol'))
+          .be.exactly(pkg);
       });
     });
 
     it('should log require errors and not fail', () => {
-      proxy.config.protocolPlugins = {
-        foo: {
-          activated: false,
-          config: {}
+      const
+        error = new Error('error'),
+        pkg = {
+          protocol: 'protocol',
+          init: sinon.spy()
         },
-        bar: {
-          activated: true,
-          config: {}
+        requireStub = sinon.stub();
+
+      requireStub.onFirstCall().returns(function () { return pkg; });
+      requireStub.onSecondCall().throws(error);
+
+      KuzzleProxy.__with__({
+        require: requireStub
+      })(() => {
+        proxy.config.protocolPlugins = {
+          foo: {
+            activated: true,
+            config: {}
+          },
+          bar: {
+            activated: true,
+            config: {}
+          }
+        };
+
+        proxy.initPlugins();
+
+        should(requireStub)
+          .be.calledTwice();
+
+        should(proxy.log.info)
+          .be.calledTwice()
+          .be.calledWith('Initializing protocol plugin foo')
+          .be.calledWith('Initializing protocol plugin bar');
+
+        should(proxy.log.error)
+          .be.calledOnce()
+          .be.calledWith('Initialization of plugin bar has failed; Reason: ');
+
+      });
+    });
+
+  });
+
+  describe('#initLogger', () => {
+    it('should support all available transports', () => {
+      proxy.config.logs.access = [{
+        level: 'level',
+        silent: 'silent',
+        colorize: 'colorize',
+        timestamp: 'timestamp',
+        json: 'json',
+        stringify: 'stringify',
+        prettyPrint: 'prettyPrint',
+        depth: 'depth',
+        showLevel: 'showLevel'
+      }];
+      proxy.config.logs.access.push(Object.assign({}, proxy.config.logs.access));
+      proxy.config.logs.errors = [Object.assign({}, proxy.config.logs.access)];
+      proxy.config.logs.errors.push(Object.assign({}, proxy.config.logs.access));
+
+      proxy.config.logs.access[0].transport = 'console';
+      proxy.config.logs.access[0].humanReadableUnhandledException = 'humanReadableUnhandledException';
+
+      proxy.config.logs.access[1].transport = 'file';
+      Object.assign(proxy.config.logs.access[1], {
+        filename: 'filename',
+        maxSize: 'maxSize',
+        maxFiles: 'maxFiles',
+        eol: 'eol',
+        logstash: 'logstash',
+        tailable: 'tailable',
+        maxRetries: 'maxRetries',
+        zippedArchive: 'zippedArchive'
+      });
+
+      proxy.config.logs.errors[0].transport = 'elasticsearch';
+      Object.assign(proxy.config.logs.errors[0], {
+        index: 'index',
+        indexPrefix: 'indexPrefix',
+        indexSuffixPattern: 'indexSuffixPattern',
+        messageType: 'messageType',
+        ensureMappingTemplate: 'ensureMappingTemplate',
+        mappingTemplate: 'mappingTemplate',
+        flushInterval: 'flushInterval',
+        clientOpts: 'clientOpts'
+      });
+
+      proxy.config.logs.errors[1].transport = 'syslog';
+      Object.assign(proxy.config.logs.errors[1], {
+        host: 'host',
+        port: 'port',
+        protocol: 'protocol',
+        path: 'path',
+        pid: 'pid',
+        facility: 'facility',
+        localhost: 'localhost',
+        type: 'type',
+        app_name: 'app_name',
+        eol: 'eol'
+      });
+
+      proxy.initLogger();
+
+      should(winstonTransportConsole)
+        .be.calledOnce()
+        .be.calledWithMatch({
+          level: 'level',
+          silent: 'silent',
+          colorize: 'colorize',
+          timestamp: 'timestamp',
+          json: 'json',
+          stringify: 'stringify',
+          prettyPrint: 'prettyPrint',
+          depth: 'depth',
+          showLevel: 'showLevel',
+          humanReadableUnhandledException: 'humanReadableUnhandledException'
+        });
+
+    });
+  });
+
+  describe('#logAccess', () => {
+    beforeEach(() => {
+      proxy.loggers = {
+        access: {
+          info: sinon.spy()
         }
-      };
-
-      proxy.initPlugins();
-
-      should(KuzzleProxy.__get__('console.log'))
-        .be.calledOnce()
-        .be.calledWith('Initializing protocol plugin bar');
-
-      should(KuzzleProxy.__get__('console.error'))
-        .be.calledOnce()
-        .be.calledWith('Initialization of plugin bar has failed; Reason: ');
+      }
     });
 
-    it('should fail if no plugin could be init', () => {
-      proxy.config.protocolPlugins = {};
-      proxy.context.pluginStore.count.returns(0);
+    it('should trigger an warn log if no connection could be found', () => {
+      proxy.logAccess(-1);
 
-      return should(() => proxy.initPlugins())
-        .throw('No plugin has been initialized properly. Shutting down.');
+      should(proxy.log.warn)
+        .be.calledOnce()
+        .be.calledWith('[access log] No connection retrieved for connection id: -1\n' +
+          'Most likely, the connection was closed before the response we received.');
+
+      should(proxy.loggers.access.info)
+        .have.callCount(0);
     });
+
+    it('should forward the params to the logger when using "logstash" format output', () => {
+      const
+        connection = { foo: 'bar' },
+        request = { foo: 'bar' },
+        error = { foo: 'bar' },
+        result = { foo: 'bar'};
+
+      proxy.clientConnectionStore.getByConnectionId = sinon.stub().returns(connection);
+      proxy.config.logs.accessLogFormat = 'logstash';
+
+      proxy.logAccess(connection, request, error, result);
+      should(proxy.loggers.access.info)
+        .be.calledOnce()
+        .be.calledWithMatch({
+          connection,
+          request,
+          error,
+          result
+      });
+    });
+
+    it('should output combined logs from an http request', () => {
+      const
+        connection = {
+          protocol: 'http',
+          headers: {
+            authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiJhZG1pbiIsImlhdCI6MTQ4MjE3MDQwNywiZXhwIjoxNDgyMTg0ODA3fQ.SmLTFuIPsVuA8Pgpf9XONW2RtxcHjQffthNZ5Er4L4s',
+            referer: 'http://referer.com',
+            'user-agent': 'user agent'
+          },
+          ips: ['1.1.1.1', '2.2.2.2']
+        },
+        request = {
+          url: 'url',
+          method: 'METHOD'
+        },
+        result = {
+          status: 'status'
+        };
+
+      proxy.clientConnectionStore.getByConnectionId = sinon.stub().returns(connection);
+      proxy.config.logs.accessLogFormat = 'combined';
+      proxy.config.logs.accessLogIpOffset = 1;
+
+      proxy.logAccess(connection, request, null, result);
+
+      should(proxy.loggers.access.info)
+        .be.calledOnce()
+        .be.calledWithMatch(/^1.1.1.1 - admin \[\d\d\/[A-Z][a-z]{2}\/\d{4}:\d\d:\d\d:\d\d [+-]\d{4}] "METHOD url HTTP" status 19 "http:\/\/referer.com" "user agent"$/);
+    });
+
+    it('should use the error status in priority', () => {
+      const
+        connection = {
+          protocol: 'websocket',
+          headers: {
+            referer: 'http://referer.com',
+            'user-agent': 'user agent'
+          },
+          ips: ['1.1.1.1', '2.2.2.2']
+        },
+        request = {
+          data: {
+            timestamp: 'timestamp',
+            requestId: 'requestId',
+            jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiJhZG1pbiIsImlhdCI6MTQ4MjE3MDQwNywiZXhwIjoxNDgyMTg0ODA3fQ.SmLTFuIPsVuA8Pgpf9XONW2RtxcHjQffthNZ5Er4L4s',
+            controller: 'controller',
+            action: 'action',
+            index: 'index',
+            collection: 'collection',
+            _id: 'id'
+          }
+        },
+        error = new Error('test'),
+        result = {
+          status: 'status'
+        };
+
+      proxy.config.logs.accessLogFormat = 'combined';
+      proxy.clientConnectionStore.getByConnectionId = sinon.stub().returns(connection);
+
+      proxy.logAccess(1, request, error, result);
+      should(proxy.loggers.access.info)
+        .be.calledOnce()
+        .be.calledWithMatch(/^2.2.2.2 - admin \[\d\d\/[A-Z][a-z]{2}\/\d{4}:\d\d:\d\d:\d\d [+-]\d{4}] "DO \/controller\/action\/index\/collection\/id WEBSOCKET" 500 19 "http:\/\/referer.com" "user agent"/)
+
+      error.status = 'ERR';
+      proxy.logAccess(1, request, error, result);
+      should(proxy.loggers.access.info)
+        .be.calledTwice();
+      should(proxy.loggers.access.info.secondCall.args[0])
+        .match(/^2.2.2.2 - admin \[\d\d\/[A-Z][a-z]{2}\/\d{4}:\d\d:\d\d:\d\d [+-]\d{4}] "DO \/controller\/action\/index\/collection\/id WEBSOCKET" ERR 19 "http:\/\/referer.com" "user agent"/)
+    });
+
   });
 
-  describe('#initBroker', () => {
-    it('should init the broker', () => {
-      proxy.initBroker();
-
-      should(proxy.context.setBroker)
-        .be.calledOnce()
-        .be.calledWith(proxy.broker);
-
-      should(proxy.broker.init)
-        .be.calledOnce()
-        .be.calledWith(proxy.context, proxy.config.backend);
-    });
-  });
-
-  describe('#initHttpProxy', () => {
-    it('should init the http proxy', () => {
-      proxy.initHttpProxy();
-
-      should(proxy.httpProxy.init)
-        .be.calledOnce()
-        .be.calledWith(proxy.context, proxy.config);
-    });
-  });
 
 });
